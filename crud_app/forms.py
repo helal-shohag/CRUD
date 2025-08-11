8b96decc2b164149afc8a447e930e3a5704dac25
helal <shohag.2896@gmail.com>
2025-08-11 18:24:09 +0600

    add

---
+++ crud_app/forms.py
@@ -0,0 +1,9 @@
+from .models import User
+from django import forms 
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name','email','password']
+        
+
---
+++ crud_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .forms import UserForm
+# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form =UserForm()        
+    return render(request, 'home.html',{'form': form})
