eb9645166d99005483d3ef254cd9383fd432d38e
helal <shohag.2896@gmail.com>
2025-08-12 18:09:28 +0600

    add

--- crud_app/views.py
+++ crud_app/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import UserForm
+from .models import User
 # Create your views here.
 def home(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            form.save()
+           name = form.cleaned_data['name']
+           email = form.cleaned_data['email']       
+           password = form.cleaned_data['password'] 
+           user = User(name=name, email=email, password=password)
+           user.save()  
     else:
-        form =UserForm()        
-    return render(request, 'home.html',{'form': form})
+        form =UserForm()
+    return render(request, 'home.html',{'form': form,'students': User.objects.all()})
+
+# def show(request):
+#     students = User.objects.all()
+#     return render(request,'home.html',{'students':students})
